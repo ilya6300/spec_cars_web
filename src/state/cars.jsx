64b8c02b5ee6d774869d807beab6206bc633e095
ilya6300 <ilya63001990@mail.ru>
2026-07-16 11:01:39 +0300

    другие машины

--- src/state/cars.jsx
+++ src/state/cars.jsx
@@ -1,5 +1,9 @@
 import policeBody from "../assets/cars/police/body.png";
 import shell_1 from "../assets/cars/police/shell_1.png";
+import redBody from "../assets/cars/\other/sport_red_body.png";
+import redWhell from "../assets/cars/\other/sport_red_wheel.png";
+import blackBody from "../assets/cars/\other/sport_black_body.png";
+import blackWhell from "../assets/cars/\other/sport_black_wheel.png";
 
 const Cars = {
   cars: [
@@ -15,6 +19,48 @@ const Cars = {
       fuel: 65000, // Топливо
       speed: "120км/ч",
       startSpeed: "3 сек",
+    },
+  ],
+  otherCars: [
+    {
+      id: 0,
+      type: "car",
+      name: "Красный гоночный автомобиль",
+      urlBody: redBody, // изображение кузова кузова
+      urlShell: redWhell, // изображение колеса
+      maxSpeed: 890,
+      mштSpeed: 700,
+      enemy: true,
+    },
+    {
+      id: 1,
+      type: "car",
+      name: "Чёрный гоночный автомобиль",
+      urlBody: blackBody, // изображение кузова кузова
+      urlShell: blackWhell, // изображение колеса
+      maxSpeed: 800,
+      mштSpeed: 650,
+      enemy: true,
+    },
+    {
+      id: 2,
+      type: "car",
+      name: "Красный автомобиль",
+      urlBody: redBody, // изображение кузова кузова
+      urlShell: redWhell, // изображение колеса
+      maxSpeed: 400,
+      mштSpeed: 300,
+      enemy: true,
+    },
+    {
+      id: 3,
+      type: "car",
+      name: "Чёрный автомобиль",
+      urlBody: blackBody, // изображение кузова кузова
+      urlShell: blackWhell, // изображение колеса
+      maxSpeed: 350,
+      mштSpeed: 200,
+      enemy: true,
     },
   ],
 };
